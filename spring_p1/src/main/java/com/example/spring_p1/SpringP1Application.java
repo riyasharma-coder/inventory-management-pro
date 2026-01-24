76043b653576dcc6309c5359f20ffa9642c25c9f
riyasharma-coder <riyasharma2864@gmail.com>
2026-01-24 21:02:59 +0530

    Fix: Added Global CORS Bean to kill 403 error

--- spring_p1/src/main/java/com/example/spring_p1/SpringP1Application.java
+++ spring_p1/src/main/java/com/example/spring_p1/SpringP1Application.java
@@ -2,6 +2,9 @@ package com.example.spring_p1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringP1Application {
@@ -10,4 +13,20 @@ public class SpringP1Application {
 		SpringApplication.run(SpringP1Application.class, args);
 	}
 
+	// This is the "Master Key" to fix the 403 Forbidden error
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(false);
+			}
+		};
+	}
 }
+
+
